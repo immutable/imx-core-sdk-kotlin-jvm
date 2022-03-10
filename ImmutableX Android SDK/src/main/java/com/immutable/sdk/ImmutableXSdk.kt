1ae20f04d427cc9d831774e224808f48ba076bca
Dom Murray <dom-murray@users.noreply.github.com>
2022-03-10 16:33:42 +1100

    feat(codegen): set up api auto generation
    APIs will auto generate from swagger file preBuild

---
+++ ImmutableX Android SDK/src/main/java/com/immutable/sdk/ImmutableXSdk.kt
@@ -0,0 +1,14 @@
+package com.immutable.sdk
+
+enum class ImmutableXBase(val url: String) {
+    Production("https://api.x.immutable.com"),
+    Ropsten("https://api.ropsten.x.immutable.com")
+}
+object ImmutableXSdk {
+    /**
+     * Sets the base property used by all Immutable X API classes.
+     */
+    fun setBase(base: ImmutableXBase) {
+        System.getProperties().setProperty("org.openapitools.client.baseUrl", base.url)
+    }
+}

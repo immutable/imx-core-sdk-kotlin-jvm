c8aa0d6ba281d40a6facc7e3dc2560c43d9b9ae6
Dom Murray <dom-murray@users.noreply.github.com>
2022-04-14 12:51:24 +1000

    [CHORE] Update docs and auto generate html

--- imx-android-sdk/src/main/java/com/immutable/sdk/model/AssetModel.kt
+++ imx-android-sdk/src/main/java/com/immutable/sdk/model/AssetModel.kt
@@ -3,8 +3,15 @@ package com.immutable.sdk.model
 import com.immutable.sdk.api.model.Token
 
 /**
- * @param quantity the amount of the asset to be transferred/sold/bought
+ * This is a wrapper for the [Token] API model that gives structured classes for the 3 types of
+ * assets (ERC20, ERC721, ETH) used in the workflow functions.
+ *
+ * @property quantity the amount of the asset to be transferred/sold/bought
  */
 sealed class AssetModel(val quantity: String) {
+    /**
+     * This converts this convenience model into the [Token] API model which will be used for the
+     * client calls.
+     */
     abstract fun toToken(): Token
 }

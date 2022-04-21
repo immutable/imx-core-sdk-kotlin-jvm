5972127d4be4e5b8a955a4f73634b4d1de505780
Natalie Bunduwongse <nf1993@gmail.com>
2022-04-21 10:09:29 +1000

    [FEAT] Maker/Taker fees, update swagger

--- imx-android-sdk/src/main/java/com/immutable/sdk/model/AssetModel.kt
+++ imx-android-sdk/src/main/java/com/immutable/sdk/model/AssetModel.kt
@@ -1,7 +1,6 @@
 package com.immutable.sdk.model
 
 import com.immutable.sdk.api.model.SignableToken
-import com.immutable.sdk.api.model.Token
 
 /**
  * This is a wrapper for the [Token] API model that gives structured classes for the 3 types of
@@ -11,10 +10,8 @@ import com.immutable.sdk.api.model.Token
  */
 sealed class AssetModel(val quantity: String) {
     /**
-     * This converts this convenience model into the [Token] API model which will be used for the
+     * This converts this convenience model into the [SignableToken] API model which will be used for the
      * client calls.
      */
-    abstract fun toToken(): Token
-
     abstract fun toSignableToken(): SignableToken
 }

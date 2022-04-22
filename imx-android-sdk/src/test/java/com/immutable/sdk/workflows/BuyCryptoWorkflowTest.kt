1f895c84749cfca2ec6c63888ca296bb0eeb8ed2
Natalie Bunduwongse <nf1993@gmail.com>
2022-04-22 10:54:58 +1000

    [FEAT] Check that wallet is registered when buying crypto

--- imx-android-sdk/src/test/java/com/immutable/sdk/workflows/BuyCryptoWorkflowTest.kt
+++ imx-android-sdk/src/test/java/com/immutable/sdk/workflows/BuyCryptoWorkflowTest.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import com.immutable.sdk.ImmutableConfig
 import com.immutable.sdk.ImmutableXBase
 import com.immutable.sdk.Signer
+import com.immutable.sdk.api.UsersApi
+import com.immutable.sdk.api.model.GetUsersApiResponse
 import com.immutable.sdk.extensions.getJson
 import com.immutable.sdk.extensions.getJsonArray
 import com.immutable.sdk.extensions.toURLEncodedString
@@ -18,6 +20,7 @@ import org.json.JSONException
 import org.json.JSONObject
 import org.junit.Before
 import org.junit.Test
+import org.openapitools.client.infrastructure.ClientException
 import java.io.IOException
 import java.net.URLEncoder
 import java.util.concurrent.CompletableFuture
@@ -25,6 +28,7 @@ import java.util.concurrent.CompletableFuture
 private const val COLOUR = 0
 private const val COLOUR_HEX = "81d8d0"
 private const val ADDRESS = "0xa76e3eeb2f7143165618ab8feaabcd395b6fac7f"
+private const val STARK_KEY = "0x01236456cb87e2c77dd6b43075c20888069a306aeff7ba36f654543aae80f678"
 private const val BUY_CRYPTO_URL = "https://buy.crypto.com"
 private const val MOONPAY_SIGNED_REQUEST = "1234567890abcdef="
 private const val TRANSACTION_ID = 8L
@@ -43,6 +47,12 @@ class BuyCryptoWorkflowTest {
 
     @MockK
     private lateinit var signer: Signer
+
+    @MockK
+    private lateinit var usersApi: UsersApi
+
+    @MockK
+    private lateinit var getUsersApiResponse: GetUsersApiResponse
 
     @MockK
     private lateinit var call: Call
@@ -71,6 +81,9 @@ class BuyCryptoWorkflowTest {
         addressFuture = CompletableFuture<String>()
         every { signer.getAddress() } returns addressFuture
         addressFuture.complete(ADDRESS)
+
+        every { usersApi.getUsers(any()) } returns getUsersApiResponse
+        every { getUsersApiResponse.accounts } returns arrayListOf(STARK_KEY)
 
         every { client.newCall(any()) } returns call
         every { call.execute() } returns response
@@ -107,7 +120,8 @@ class BuyCryptoWorkflowTest {
             signer = signer,
             client = client,
             colourInt = COLOUR,
-            colourCodeHex = "#$COLOUR_HEX"
+            colourCodeHex = "#$COLOUR_HEX",
+            usersApi = usersApi
         )
     }
 
@@ -175,6 +189,25 @@ class BuyCryptoWorkflowTest {
     @Test(expected = JSONException::class)
     fun testBuyCryptoFailedOnGetBuyCryptoUrl() {
         every { jsonObject.getString(SIGNATURE) } throws JSONException("")
+
+        buyCrypto()
+
+        verify(exactly = 0) { Utils.launchCustomTabs(any(), any(), any()) }
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun testBuyCryptoWalletNotRegistered_notFound() {
+        every { getUsersApiResponse.accounts } throws
+            ClientException(statusCode = HTTP_STATUS_CODE_NOT_FOUND)
+
+        buyCrypto()
+
+        verify(exactly = 0) { Utils.launchCustomTabs(any(), any(), any()) }
+    }
+
+    @Test(expected = IllegalStateException::class)
+    fun testBuyCryptoWalletNotRegistered_emptyList() {
+        every { getUsersApiResponse.accounts } returns emptyList()
 
         buyCrypto()
 

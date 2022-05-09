ee84343b9dc4803d635a5578a02b215bf318bccf
Dom Murray <dom-murray@users.noreply.github.com>
2022-05-09 11:28:54 +1000

    [REFACTOR] Rename project and modules

--- addHeaderScript.py
+++ addHeaderScript.py
@@ -1,8 +1,8 @@
 import fileinput
 import sys
 
-filename = './imx-android-sdk/generated/src/main/kotlin/org/openapitools/client/infrastructure/ApiClient.kt'
+filename = './imx-core-sdk-android/generated/src/main/kotlin/org/openapitools/client/infrastructure/ApiClient.kt'
 for i, line in enumerate(fileinput.input(filename, inplace=1)):
     sys.stdout.write(line.replace('builder.build()',
-                                  'builder.addInterceptor {\n\t\t\t\tit.proceed(\n\t\t\t\t\tit.request().newBuilder()\n\t\t\t\t\t\t.addHeader("x-sdk-version", "imx-android-sdk-{}")\n\t\t\t\t\t\t.build()\n\t\t\t\t)\n\t\t\t}.build()'.replace(
+                                  'builder.addInterceptor {\n\t\t\t\tit.proceed(\n\t\t\t\t\tit.request().newBuilder()\n\t\t\t\t\t\t.addHeader("x-sdk-version", "imx-core-sdk-android-{}")\n\t\t\t\t\t\t.build()\n\t\t\t\t)\n\t\t\t}.build()'.replace(
                                       "{}", sys.argv[1])))

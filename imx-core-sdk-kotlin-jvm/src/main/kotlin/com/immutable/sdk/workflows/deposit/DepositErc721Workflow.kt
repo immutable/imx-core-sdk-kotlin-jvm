e403214014258fee17d8dfd24f10de9d79397983
Natalie Bunduwongse <nf1993@gmail.com>
2022-10-18 11:25:51 +1100

    feat: deposit ERC721

--- imx-core-sdk-kotlin-jvm/src/main/kotlin/com/immutable/sdk/workflows/deposit/DepositErc721Workflow.kt
+++ imx-core-sdk-kotlin-jvm/src/main/kotlin/com/immutable/sdk/workflows/deposit/DepositErc721Workflow.kt
@@ -9,12 +9,13 @@ import com.immutable.sdk.api.EncodingApi
 import com.immutable.sdk.api.UsersApi
 import com.immutable.sdk.api.model.GetSignableRegistrationResponse
 import com.immutable.sdk.contracts.Core_sol_Core
-import com.immutable.sdk.contracts.IERC20_sol_IERC20
+import com.immutable.sdk.contracts.IERC721_sol_IERC721
+import com.immutable.sdk.contracts.Registration_sol_Registration
 import com.immutable.sdk.extensions.hexRemovePrefix
 import com.immutable.sdk.extensions.hexToByteArray
-import com.immutable.sdk.model.Erc20Asset
-import com.immutable.sdk.model.formatQuantity
-import com.immutable.sdk.workflows.executeDeposit
+import com.immutable.sdk.model.Erc721Asset
+import com.immutable.sdk.workflows.executeDepositToken
+import com.immutable.sdk.workflows.executeRegisterAndDepositToken
 import com.immutable.sdk.workflows.prepareDeposit
 import com.immutable.sdk.workflows.sendTransaction
 import org.web3j.protocol.Web3j
@@ -25,10 +26,10 @@ import org.web3j.tx.gas.DefaultGasProvider
 import java.util.concurrent.CompletableFuture
 
 @Suppress("LongParameterList", "LongMethod")
-internal fun depositErc20(
+internal fun depositErc721(
     base: ImmutableXBase,
     nodeUrl: String,
-    token: Erc20Asset,
+    token: Erc721Asset,
     signer: Signer,
     depositsApi: DepositsApi,
     usersApi: UsersApi,
@@ -38,17 +39,24 @@ internal fun depositErc20(
 
     val web3j = Web3j.build(HttpService(nodeUrl))
 
-    approveErc20Token(base, token, web3j, signer)
+    approveErc721Token(base, token, web3j, signer)
         .thenCompose { prepareDeposit(base, nodeUrl, token, signer, depositsApi, usersApi, encodingApi) }
         .thenCompose { params ->
-            executeDeposit(
-                base,
-                nodeUrl,
-                signer,
-                params,
-                registerAndDepositFunction = Core_sol_Core.FUNC_REGISTERANDDEPOSITERC20,
-                registerAndDepositData = { contract: Core_sol_Core, response: GetSignableRegistrationResponse ->
-                    contract.registerAndDepositERC20(
+            // Using individual execute functions instead of `executeDeposit` since registration contract
+            // is required (instead of core contract) to register and deposit ERC 721 tokens
+            if (!params.isRegistered) executeRegisterAndDepositToken(
+                web3j = web3j,
+                signer = signer,
+                params = params,
+                contract = Registration_sol_Registration.load(
+                    ImmutableConfig.getRegistrationContractAddress(base),
+                    web3j,
+                    ClientTransactionManager(web3j, params.address),
+                    DefaultGasProvider()
+                ),
+                contractFunction = Registration_sol_Registration.FUNC_REGISTERANDDEPOSITNFT,
+                data = { contract: Registration_sol_Registration, response: GetSignableRegistrationResponse ->
+                    contract.registerAndDepositNft(
                         params.address,
                         params.starkKey.hexRemovePrefix().toBigInteger(HEX_RADIX),
                         response.operatorSignature.hexToByteArray(),
@@ -57,16 +65,27 @@ internal fun depositErc20(
                         params.amount
                     ).encodeFunctionCall()
                 },
-                depositFunction = Core_sol_Core.FUNC_DEPOSITERC20,
-                depositData = { contract: Core_sol_Core ->
-                    contract.depositERC20(
+                usersApi = usersApi
+            )
+            else executeDepositToken(
+                web3j = web3j,
+                signer = signer,
+                params = params,
+                contract = Core_sol_Core.load(
+                    ImmutableConfig.getCoreContractAddress(base),
+                    web3j,
+                    ClientTransactionManager(web3j, params.address),
+                    DefaultGasProvider()
+                ),
+                contractFunction = Core_sol_Core.FUNC_DEPOSITNFT,
+                data = { contract: Core_sol_Core ->
+                    contract.depositNft(
                         params.starkKey.hexRemovePrefix().toBigInteger(HEX_RADIX),
                         params.assetType,
                         params.vaultId,
-                        params.amount
+                        token.tokenId.toBigInteger()
                     ).encodeFunctionCall()
-                },
-                usersApi
+                }
             )
         }
         .whenComplete { response, throwable ->
@@ -80,14 +99,14 @@ internal fun depositErc20(
 /**
  * Approve whether an amount of token from an account can be spent by a third-party account
  */
-private fun approveErc20Token(
+private fun approveErc721Token(
     base: ImmutableXBase,
-    token: Erc20Asset,
+    token: Erc721Asset,
     web3j: Web3j,
     signer: Signer
 ): CompletableFuture<EthSendTransaction> = signer.getAddress()
     .thenCompose { address ->
-        val contract = IERC20_sol_IERC20.load(
+        val contract = IERC721_sol_IERC721.load(
             token.tokenAddress,
             web3j,
             ClientTransactionManager(web3j, address),
@@ -96,10 +115,10 @@ private fun approveErc20Token(
 
         sendTransaction(
             contract = contract,
-            contractFunction = IERC20_sol_IERC20.FUNC_APPROVE,
+            contractFunction = IERC721_sol_IERC721.FUNC_APPROVE,
             data = contract.approve(
                 ImmutableConfig.getCoreContractAddress(base),
-                token.formatQuantity().toBigInteger()
+                token.tokenId.toBigInteger()
             ).encodeFunctionCall(),
             signer = signer,
             web3j = web3j

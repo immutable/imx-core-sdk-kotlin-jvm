f1837bcadafacc53b5abdbedabb79f8786752c6c
Natalie Bunduwongse <nf1993@gmail.com>
2022-11-25 13:10:59 +1100

    feat: generate legacy key pair (#134)
    * feat: add method to deterministically generate stark private key
    
    * refactor: generate legacy stark private key function name

--- imx-core-sdk-kotlin-jvm/src/test/kotlin/com/immutable/sdk/crypto/StarkKeyTest.kt
+++ imx-core-sdk-kotlin-jvm/src/test/kotlin/com/immutable/sdk/crypto/StarkKeyTest.kt
@@ -1,16 +1,41 @@
 package com.immutable.sdk.crypto
 
 import com.immutable.sdk.Constants
+import com.immutable.sdk.Signer
 import io.mockk.*
+import io.mockk.impl.annotations.MockK
 import org.bouncycastle.crypto.AsymmetricCipherKeyPair
 import org.bouncycastle.crypto.generators.ECKeyPairGenerator
 import org.bouncycastle.crypto.params.ECPrivateKeyParameters
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 import org.web3j.crypto.ECKeyPair
 import java.math.BigInteger
 
 class StarkKeyTest {
+    @MockK
+    lateinit var signer: Signer
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
+    @Test
+    fun testGetAccountPath() {
+        val path = StarkKey.getAccountPath("0xa76e3eeb2f7143165618ab8feaabcd395b6fac7f")
+        assertEquals("m/2645'/579218131'/211006541'/1534045311'/1431804530'/1", path)
+    }
+
+    @Test
+    fun testGetIntFromBits() {
+        // binary representation: 00010010001101001010011001001001101111111101100001001111111101000101001000100010
+        val hex = "1234a649bfd84ff45222"
+        assertEquals(34, StarkKey.getIntFromBits(hex, 9)) // 000100010
+        assertEquals(1_024_657, StarkKey.getIntFromBits(hex, 25, 5)) // 11111010001010010001
+    }
+
     @Test
     fun testHashSha256Update() {
         assertEquals(
@@ -78,5 +103,15 @@ class StarkKeyTest {
             StarkCurve.generatePrivateKey()
             StarkCurve.getKeyPair(any())
         }
+    }
+
+    @Test
+    fun testGenerateLegacyKeyPair() {
+        val pk = StarkKey.generateLegacyStarkPrivateKey(
+            "0xe834136cc3206a8f80acd81922d80b377ca769dc973d83ee2bd8bed4b7cdc3565f2a3aded8de5c93f85" +
+                "327d5c1fb535959bdc3068318875b95788b074f3ab2931c",
+            "0x2cD7944D8398017d0D142Ea2Ec483bc230f01A84"
+        )
+        assertEquals("0512653c071aa6fb61615354cb850c1d6c122635c08329ce3b5c1f23ec844d19", pk)
     }
 }
